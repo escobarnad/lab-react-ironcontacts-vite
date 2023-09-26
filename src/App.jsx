35642750d2f63b158a17caf61bb792fde9334c99
JanJaap <j-jschenk@hotmail.com>
2023-09-26 12:26:48 +0200

    basic working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import "./App.css";
+import contactsJSON from './contacts.json'
 
 function App() {
-  return (
+const [contacts, setContacts] = useState(contactsJSON.slice(0,5))
+  
+return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
-    </div>
-  );
-}
-
+      <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              </tr>
+              </thead>
+              <tbody>
+      {contacts.map((oneContact) => {
+       return (
+       <tr key={oneContact.id}>
+          <td>
+            <img src={oneContact.pictureUrl} style={{height: "200px"}} />
+              </td>
+              <td>{oneContact.name}</td>
+              <td>{oneContact.popularity}</td>
+              </tr>
+       );
+            })}
+            </tbody>
+            </table>
+            </div>
+)
+          }
 export default App;
